fb2842147eda8176c5cbc7641a6d7ad20e549816
alex-bikbaeva <aleksandra.bikbaeva@axiomjdk.ru>
2026-04-08 05:00:13 +0300

    modify replaceActor() method and String to Person
    * modify replaceActor() method and String to Person
    
    * Delete Actor.java
    
    * Delete Ballet.java
    
    * Delete Director.java
    
    * Delete Gender.java
    
    * Delete MusicalShow.java
    
    * Delete Opera.java
    
    * Delete Person.java
    
    * Delete Show.java
    
    * Delete Theatre.java

--- src/MusicalShow.java
+++ src/MusicalShow.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class MusicalShow extends Show {
-    private String musicAuthor;
+    private Person musicAuthor;
     private String librettoText;
 
-    public MusicalShow(String title, double duration, Director director, ArrayList<Actor> listOfActors, String musicAuthor, String librettoText) {
+    public MusicalShow(String title, double duration, Director director, ArrayList<Actor> listOfActors, Person musicAuthor, String librettoText) {
         super(title, duration, director, listOfActors);
         this.musicAuthor = musicAuthor;
         this.librettoText = librettoText;
     }
 
-    public String getLibrettoText() {
-        return librettoText;
+    public void printLibrettoText() {
+        System.out.println(librettoText);
     }
 }
